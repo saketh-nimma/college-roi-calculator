d948c96ed2ae6d2b1a95ac6f22e4044884007da4
saketh-nimma <sakethnimma3@gmail.com>
2026-01-13 22:13:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+let chart;
+
+function calculate() {
+  const cost = Number(document.getElementById("cost").value);
+  const years = Number(document.getElementById("years").value);
+  const salary = Number(document.getElementById("salary").value);
+  const rate = Number(document.getElementById("rate").value) / 100;
+
+  const totalCost = cost * years;
+  const annualLoan = totalCost * rate;
+  const netIncome = salary - annualLoan;
+  const breakEven = totalCost / netIncome;
+
+  document.getElementById("output").innerHTML = `
+    Total Cost: $${totalCost.toLocaleString()}<br>
+    Annual Loan Payment: $${annualLoan.toLocaleString()}<br>
+    Net Annual Income: $${netIncome.toLocaleString()}<br>
+    Break-even Time: ${breakEven.toFixed(1)} years
+  `;
+
+  buildChart(totalCost, netIncome);
+}
+
+function buildChart(cost, income) {
+  const years = [];
+  const earnings = [];
+  const debt = [];
+
+  let totalEarned = 0;
+
+  for (let i = 1; i <= 10; i++) {
+    years.push(i);
+    totalEarned += income;
+    earnings.push(totalEarned);
+    debt.push(cost);
+  }
+
+  if (chart) chart.destroy();
+
+  chart = new Chart(document.getElementById("roiChart"), {
+    type: "line",
+    data: {
+      labels: years,
+      datasets: [
+        {
+          label: "Cumulative Earnings",
+          data: earnings,
+          borderWidth: 2
+        },
+        {
+          label: "College Cost",
+          data: debt,
+          borderWidth: 2
+        }
+      ]
+    }
+  });
+}
